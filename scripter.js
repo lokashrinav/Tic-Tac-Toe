5ffb43f16fdf04930910c0963edf08087a9d5a0f
Shrinav Loka <lokashrinav@gmail.com>
2022-09-30 16:57:50 -0400

    Fixing mobile error

--- scripter.js
+++ scripter.js
@@ -46,7 +46,17 @@ var Gameboard = (function() {
                 xl--;
                 Gameboard.statement()
             }
-            
+            else{
+                Gameboard.checker()
+            }
+
+        },
+        checker: function(){
+            if(arr[0] != "" && arr[1] != "" && arr[2] != "" && arr[3] != "" && arr[4] != "" && arr[5] != "" && arr[6] != "" && arr[7] != "" && arr[8] != "" ) {
+                popup.classList.remove("popup")
+                popup.classList.add("popup-open")
+                words.innerHTML = "It's a Tie!"
+            }
         },
         statement: function(r){
             if(xl>0){
